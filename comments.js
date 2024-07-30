46da3ba23711da72dc5472cb08327d18e62fa8c7
dotnetshopllc <125929225+dotnetshopllc@users.noreply.github.com>
2024-07-30 21:39:53 +0000

    Copilot third  commit

--- comments.js
+++ comments.js
@@ -1,24 +1,2 @@
 // create web server    
-const express = require('express');
-const app = express();
-const port = 3000;
-
-// create a middleware function
-app.use((req, res, next) => {
-  console.log('Middleware function called');
-  next();
-});
-
-// create a route
-app.get('/', (req, res) => {
-  res.send('This is the home page');
-});
-
-// create a route
-app.get('/about', (req, res) => {
-  res.send('This is the about page');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+var http = require('http');
